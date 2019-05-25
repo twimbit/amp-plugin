fa0558a7fbd2e4f2ebb5afdb22bc1fb49fefe856
Dhruva.atlancey <dhruva.atlancey@gmail.com>
2019-05-25 12:42:26 +0530

    dhruva code added

--- wp-content/plugins/amp-wp-1.2-beta1-built/includes/templates/single-amp_story.php
+++ wp-content/plugins/amp-wp-1.2-beta1-built/includes/templates/single-amp_story.php
@@ -21,9 +21,54 @@ the_post();
 		<?php rel_canonical(); ?>
 		<?php amp_add_generator_metadata(); ?>
 		<style amp-boilerplate>body{-webkit-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-moz-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-ms-animation:-amp-start 8s steps(1,end) 0s 1 normal both;animation:-amp-start 8s steps(1,end) 0s 1 normal both}@-webkit-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-moz-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-ms-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-o-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}</style><noscript><style amp-boilerplate>body{-webkit-animation:none;-moz-animation:none;-ms-animation:none;animation:none}</style></noscript>
-	</head>
-	<body>
-		<?php
+        <style amp-custom="">
+
+            .amp-sidebar-toolbar-target-shown {
+                display: none;
+            }
+        </style>
+
+        <script async custom-element="amp-sidebar" src="https://cdn.ampproject.org/v0/amp-sidebar-0.1.js"></script>
+
+        <script>
+            function myunction() {
+               location.replace("https://www.w3schools.com")
+            }
+        </script>
+
+        <script type="text/javascript">
+            function getUrl()
+            {
+                location.replace("https://www.w3schools.com")
+            }
+            }
+
+        </script>
+
+
+    </head>
+
+
+
+    <body >
+
+    <?php
+    $next_post_url = get_permalink( get_adjacent_post(false,'',false)->ID );
+    $previous_post_url = get_permalink( get_adjacent_post(false,'',true)->ID );
+    ?>
+
+
+    <!--  <button style="margin-bottom: -10px;left: 10px;z-index: 10;position: fixed;">TEXT BB</button>    -->
+    <a href='<?php echo $previous_post_url ?>'
+       style="
+    margin-bottom: -10px;
+    left: 10px;
+    z-index: 10;
+    position: fixed;"><img src=home.png width="512" height="512" title="Go  home" alt="Go home"></a>
+    <!-- //imgsrc insert https://image.shutterstock.com/image-vector/home-icon-isolated-on-transparent-450w-654663997.jpg https://image.flaticon.com/icons/svg/149/149445.svg  -->
+    <!-- <img src="https://image.shutterstock.com/image-vector/home-icon-isolated-on-transparent-450w-654663997.jpg" width="42" height="46" title="Go  home" alt="Go home">   -->
+
+  <?php
 		$metadata = amp_get_schemaorg_metadata();
 		if ( isset( $metadata['publisher']['logo']['url'] ) ) {
 			$publisher_logo_src = $metadata['publisher']['logo']['url']; // @todo Use amp-publisher-logo.
@@ -38,6 +83,7 @@ the_post();
 		$poster_square    = wp_get_attachment_image_url( $thumbnail_id, AMP_Story_Post_Type::STORY_SQUARE_IMAGE_SIZE );
 		$poster_landscape = wp_get_attachment_image_url( $thumbnail_id, AMP_Story_Post_Type::STORY_LANDSCAPE_IMAGE_SIZE );
 		?>
+
 		<amp-story
 			standalone
 			publisher-logo-src="<?php echo esc_url( $publisher_logo_src ); ?>"
@@ -51,12 +97,48 @@ the_post();
 				poster-landscape-src="<?php echo esc_url( $poster_landscape ); ?>"
 			<?php endif; ?>
 		>
-			<?php the_content(); ?>
-		</amp-story>
+
+
+
+            <amp-sidebar id="sidebar2" layout="nodisplay" side="right">
+                <ul>
+                    <li>Nav item 1</li>
+                    <li><a href="#idFour" on="tap:idFour.scrollTo">Nav item 4</a></li>
+                    <li><BUTTON id="ub" onclick="myunction()">Click me</BUTTON></li>
+                    <li>Nav item 6</li>
+                </ul>
+                <nav toolbar="(max-width: 400px)" toolbar-target="target-element">
+
+                </nav>
+            </amp-sidebar>
+
+                <amp-sidebar id="sidebar1" layout="nodisplay" side="right">
+                <ul>
+                    <li>Nav item 1</li>
+                    <li><a href="#idTwo" on="tap:idTwo.scrollTo">Nav item 2</a></li>
+                    <li><a onclick="myunction()">Nav item 3</a></li>
+                    <li><a href="#idFour" on="tap:idFour.scrollTo">Nav item 4</a></li>
+                    <li><BUTTON id="ub" onclick="myunction()">Click me</BUTTON></li>
+                    <li>Nav item 6</li>
+                </ul>
+
+                <nav toolbar="(max-width: 400px)" toolbar-target="target-element">
+                </nav>
+            </amp-sidebar>
+
+
+			<?php
+           echo the_content();
+            ?>
+        </amp-story>
 
 		<?php
 		// Note that \AMP_Story_Post_Type::filter_frontend_print_styles_array() will limit which styles are printed.
 		print_late_styles();
 		?>
-	</body>
+
+   
+
+</body>
+
 </html>

5d7efd6bc0f4cd0323198f4ff1c50e01e0597639
aman_razdan <aman111334@gmail.com>
2019-08-21 12:31:25 +0530

    small change

--- wp-content/plugins/Amp-plugin/includes/templates/single-amp_story.php
+++ wp-content/plugins/Amp-plugin/includes/templates/single-amp_story.php
@@ -24,8 +24,15 @@ $metadata = amp_get_schemaorg_metadata();
 		<?php amp_add_generator_metadata(); ?>
 		<script type="application/ld+json"><?php echo wp_json_encode( $metadata, JSON_UNESCAPED_UNICODE ); ?></script>
 		<style amp-boilerplate>body{-webkit-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-moz-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-ms-animation:-amp-start 8s steps(1,end) 0s 1 normal both;animation:-amp-start 8s steps(1,end) 0s 1 normal both}@-webkit-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-moz-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-ms-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-o-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}</style><noscript><style amp-boilerplate>body{-webkit-animation:none;-moz-animation:none;-ms-animation:none;animation:none}</style></noscript>
-	</head>
-	<body>
+
+
+</head>
+
+
+
+<body>
+
+
 		<?php
 		if ( isset( $metadata['publisher']['logo']['url'] ) ) {
 			$publisher_logo_src = $metadata['publisher']['logo']['url'];
@@ -36,42 +43,158 @@ $metadata = amp_get_schemaorg_metadata();
 		}
 		$publisher = isset( $metadata['publisher']['name'] ) ? $metadata['publisher']['name'] : get_option( 'blogname' );
 
-		$meta_images = AMP_Story_Media::get_story_meta_images();
-		?>
-		<amp-story
-			standalone
-			<?php
-			/**
-			 * Filters whether the story supports landscape.
-			 *
-			 * @param bool    $supports_landscape Whether supports landscape. Currently false by default, but this will change in the future (e.g. via user toggle).
-			 * @param wp_Post $post               The current amp_story post object.
-			 */
-			if ( apply_filters( 'amp_story_supports_landscape', false, get_post() ) ) {
-				echo 'supports-landscape';
+// There is a fallback poster-portrait image added via a filter, in case there's no featured image.
+$thumbnail_id     = get_post_thumbnail_id();
+$poster_portrait  = wp_get_attachment_image_url( $thumbnail_id, AMP_Story_Post_Type::STORY_CARD_IMAGE_SIZE );
+$poster_square    = wp_get_attachment_image_url( $thumbnail_id, AMP_Story_Post_Type::STORY_SQUARE_IMAGE_SIZE );
+$poster_landscape = wp_get_attachment_image_url( $thumbnail_id, AMP_Story_Post_Type::STORY_LANDSCAPE_IMAGE_SIZE );
+?>
+<amp-story
+        standalone
+	<?php
+	/**
+	 * Filters whether the story supports landscape.
+	 *
+	 * @param bool    $supports_landscape Whether supports landscape. Currently false by default, but this will change in the future (e.g. via user toggle).
+	 * @param wp_Post $post               The current amp_story post object.
+	 */
+	if ( apply_filters( 'amp_story_supports_landscape', false, get_post() ) ) {
+		echo 'supports-landscape';
+	}
+	?>
+        publisher-logo-src="<?php echo esc_url( $publisher_logo_src ); ?>"
+        publisher="<?php echo esc_attr( $publisher ); ?>"
+        title="<?php the_title_attribute(); ?>"
+        poster-portrait-src="<?php echo esc_url( $poster_portrait ); ?>"
+	<?php if ( $poster_square ) : ?>
+        poster-square-src="<?php echo esc_url( $poster_square ); ?>"
+	<?php endif; ?>
+	<?php if ( $poster_landscape ) : ?>
+        poster-landscape-src="<?php echo esc_url( $poster_landscape ); ?>"
+	<?php endif; ?>
+>
+	<?php
+	amp_print_story_auto_ads();
+	the_content();
+	amp_print_analytics( '' );
+	?>
+    <amp-story-bookend layout=nodisplay>
+        <script type="application/json">
+            {
+                "bookendVersion": "v1.0",
+                "shareProviders": [
+                    "linkedin",
+                    "whatsapp",
+                    "Twitter"
+                ],
+                "components": [
+                    {
+                        "type": "heading",
+                        "text": "Up Next"
+                    }
+<?php
+			// to loop post in bookend
+			$loop_length=3;  //specify number of posts ahead you want to display
+			$check_help=0;    //require as a checkpoint to follow different paths in loop
+			global $post;
+			$revert_post_content=$post;  //save current global post content to setback changes to current post after loop execution ends
+			for($loop_start = 1; $loop_start <= $loop_length; $loop_start++)
+			{
+				if( $loop_start === 1)
+				{
+					$nextPost = get_next_post();
+				}
+				else
+				{
+					global $post;
+					$post = get_next_post();
+					setup_postdata( $post );
+
+					if($check_help===1)
+					{
+						$nextPost = $post;
+						$check_help=2;
+					}
+					else
+					{
+						$nextPost = get_next_post();
+						$check_help=0;
+					}
+				}
+				if(get_permalink($nextPost) != get_permalink($post))   // to check when last post arrives to loop back to first posts
+				{
+				}
+				else
+				{
+					// getting the link of oldest story (1st story)
+					$args = array(
+						'numberposts'      => 2,
+						'category'         => 0,
+						'orderby'          => 'date',
+						'order'            => 'ASC', // the 1st array element will be 1st story(oldest story)
+						'include'          => array(),
+						'exclude'          => array(),
+						'meta_key'         => '',
+						'meta_value'       => '',
+						'post_type'        => 'amp_story',
+						'suppress_filters' => true,
+					);
+					$get_post_for_story=get_posts($args);
+					$first_story=$get_post_for_story[0]; // 0 will give the 1st  story here ( oldest story)
+
+					global $post;
+					$post = $first_story;
+					setup_postdata( $post );
+					if($check_help!=0 )
+					{
+						$nextPost = get_next_post();
+					}
+					else
+					{
+						$nextPost = $post;
+						$check_help=1;
+					}
+
+				}
+
+
+				// to stop looping of stories
+				if(get_permalink($revert_post_content)!== get_permalink($nextPost))
+				{
+					echo ',{';
+					if ( $loop_start === 1 ) {
+						echo ' "type": "landscape", ';
+					} else {
+						echo ' "type": "small", ';
+					}
+					echo '"title": "';
+					echo $nextPost->post_title;
+					echo '",';
+					echo '"url": "';
+					echo get_permalink( $nextPost );
+					echo '",';
+					echo '"image": "';
+					echo get_the_post_thumbnail_url( $nextPost->ID );
+					echo '"';
+					echo '}';
+				}
+				else{
+					$loop_start=$loop_length+2;
+				}
+				if( $loop_start >= $loop_length)
+				{ //wp_reset_postdata();
+					$post=$revert_post_content;}
 			}
 			?>
-			publisher-logo-src="<?php echo esc_url( $publisher_logo_src ); ?>"
-			publisher="<?php echo esc_attr( $publisher ); ?>"
-			title="<?php the_title_attribute(); ?>"
-			poster-portrait-src="<?php echo esc_url( $meta_images['poster-portrait'] ); ?>"
-			<?php if ( isset( $meta_images['poster-square'] ) ) : ?>
-				poster-square-src="<?php echo esc_url( $meta_images['poster-square'] ); ?>"
-			<?php endif; ?>
-			<?php if ( isset( $meta_images['poster-landscape'] ) ) : ?>
-				poster-landscape-src="<?php echo esc_url( $meta_images['poster-landscape'] ); ?>"
-			<?php endif; ?>
-		>
-			<?php
-			amp_print_story_auto_ads();
-			the_content();
-			amp_print_analytics( '' );
-			?>
-		</amp-story>
+  ]
+      }
+     </script>
+    </amp-story-bookend>
+</amp-story>
 
-		<?php
-		// Note that \AMP_Story_Post_Type::filter_frontend_print_styles_array() will limit which styles are printed.
-		print_late_styles();
-		?>
-	</body>
+<?php
+// Note that \AMP_Story_Post_Type::filter_frontend_print_styles_array() will limit which styles are printed.
+print_late_styles();
+?>
+</body>
 </html>
